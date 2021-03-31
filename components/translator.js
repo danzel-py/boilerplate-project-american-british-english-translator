189dffa05fc9997ae5916bae1927a65dd1c6a85f
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-03-31 16:52:54 +0700

    Time format added

--- components/translator.js
+++ components/translator.js
@@ -19,56 +19,75 @@ document.getElementById("demo").innerHTML = lol;
 class Translator {
     americanToBritish(text) {
         let newText = text
-        console.log(newText)
         for (const [key, value] of Object.entries(americanOnly)) {
             let toSearch = key
             let toPlace = value
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, go next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, go next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
         for (const [key, value] of Object.entries(americanToBritishSpelling)) {
             let toSearch = key
             let toPlace = value
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, go next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, go next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
         for (const [key, value] of Object.entries(americanToBritishTitles)) {
             let toSearch = key
             let toPlace = value
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, go next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, go next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
+        // replace 12:15 to 12.15
+        let regex = /[0-1][0-9][:][0-5][0-9]|[2][0-4][:][0-5][0-9]|[0-9][:][0-5][0-9]/gm
+        let found = newText.match(regex)
+        if (found.length < 1) return (newText) // not found, do nothing
+        found.forEach(clock => {
+            let toSearch = clock
+            let toPlace = clock.replace(':', '.')
+            let index = newText.search(toSearch)
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
+        })
         return (newText)
     }
-    
+
     britishToAmerican(text) {
         let newText = text
-        console.log(newText)
         for (const [key, value] of Object.entries(britishOnly)) {
             let toSearch = key
             let toPlace = value
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
         for (const [key, value] of Object.entries(americanToBritishSpelling)) {
             let toSearch = value
             let toPlace = key
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
         for (const [key, value] of Object.entries(americanToBritishTitles)) {
             let toSearch = value
             let toPlace = key
             let index = newText.search(toSearch)
-            if(index<0) {continue} // not found, next iteration
-            newText = newText.slice(0,index) + toPlace + newText.slice(index+toSearch.length)
+            if (index < 0) continue // not found, next iteration
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
         }
-        return (newText)
+        // Replace 12.15 to 12:15
+        let regex = /[0-1][0-9][.][0-5][0-9]|[2][0-4][.][0-5][0-9]|[0-9][.][0-5][0-9]/gm
+        let found = newText.match(regex)
+        if (found.length < 1) return (newText) // not found, do nothing
+        found.forEach(clock => {
+            let toSearch = clock
+            let toPlace = clock.replace('.', ':')
+            let index = newText.search(toSearch)
+            newText = newText.slice(0, index) + toPlace + newText.slice(index + toSearch.length)
+        })
+        return(newText)
+
     }
 }
 
