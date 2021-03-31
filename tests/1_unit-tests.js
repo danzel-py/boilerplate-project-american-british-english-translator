c4a6c250cd65a08cd13c586ed71fe1dcc8387a59
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-04-01 06:36:59 +0700

    Added unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -65,23 +65,91 @@ suite('Unit Tests', () => {
             `Lunch is at <span class = 'highlight'>12.15</span> today.`
         )
     })
+    //------------------------------------------------------------------
+    test('Translate We watched the footie match for a while. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('We watched the footie match for a while.'),
+            `We watched the <span class = 'highlight'>soccer</span> match for a while.`
+        )
+    })
+    test('Translate Paracetamol takes up to an hour to work. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('Paracetamol takes up to an hour to work.'),
+            `<span class = 'highlight'>Tylenol</span> takes up to an hour to work.`
+        )
+    })
+    test('Translate First, caramelise the onions. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('First, caramelise the onions.'),
+            `First, <span class = 'highlight'>caramelize</span> the onions.`
+        )
+    })
+    test('Translate I spent the bank holiday at the funfair. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('I spent the bank holiday at the funfair.'),
+            `I spent the <span class = 'highlight'>public holiday</span> at the <span class = 'highlight'>carnival</span>.`
+        )
+    })
+    test('Translate I had a bicky then went to the chippy. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('I had a bicky then went to the chippy.'),
+            `I had a <span class = 'highlight'>cookie</span> then went to the <span class = 'highlight'>fish-and-chip shop</span>.`
+        )
+    })
+    test('Translate I\'ve just got bits and bobs in my bum bag. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('I\'ve just got bits and bobs in my bum bag.'),
+            `I've just got <span class = 'highlight'>odds and ends</span> in my <span class = 'highlight'>fanny pack</span>.`
+        )
+    })
+    test('Translate The car boot sale at Boxted Airfield was called off. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('The car boot sale at Boxted Airfield was called off.'),
+            `The <span class = 'highlight'>swap meet</span> at Boxted Airfield was called off.`
+        )
+    })
+    test('Translate Have you met Mrs Kalyani? to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('Have you met Mrs Kalyani?'),
+            `Have you met <span class = 'highlight'>Mrs.</span> Kalyani?`
+        )
+    })
+    test('Translate Prof Joyner of King\'s College, London. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('Prof Joyner of King\'s College, London.'),
+            `<span class = 'highlight'>Prof.</span> Joyner of King's College, London.`
+        )
+    })
+    test('Translate Tea time is usually around 4 or 4.30. to American English',()=>{
+        assert.equal(
+            translator.britishToAmerican('Tea time is usually around 4 or 4.30.'),
+            `Tea time is usually around 4 or <span class = 'highlight'>4:30</span>.`
+        )
+    })
+    test('Highlight translation in Mangoes are my favorite fruit.', () => {
+        assert.equal(
+            translator.americanToBritish('Mangoes are my favorite fruit.'),
+            "Mangoes are my <span class = 'highlight'>favourite</span> fruit."
+        )
+    })
+    test('Highlight translation in I ate yogurt for breakfast.',()=>{
+        assert.equal(
+            translator.americanToBritish('I ate yogurt for breakfast.'),
+            "I ate <span class = 'highlight'>yoghurt</span> for breakfast."
+        )
+    })
+    test('Highlight translation in We watched the footie match for a while.',()=>{
+        assert.equal(
+            translator.britishToAmerican('We watched the footie match for a while.'),
+            `We watched the <span class = 'highlight'>soccer</span> match for a while.`
+        )
+    })
+    test('Highlight translation in Paracetamol takes up to an hour to work.',()=>{
+        assert.equal(
+            translator.britishToAmerican('Paracetamol takes up to an hour to work.'),
+            `<span class = 'highlight'>Tylenol</span> takes up to an hour to work.`
+        )
+    })
+    
 
 });
-
-/*
-
-Translate We watched the footie match for a while. to American English
-Translate Paracetamol takes up to an hour to work. to American English
-Translate First, caramelise the onions. to American English
-Translate I spent the bank holiday at the funfair. to American English
-Translate I had a bicky then went to the chippy. to American English
-Translate I've just got bits and bobs in my bum bag. to American English
-Translate The car boot sale at Boxted Airfield was called off. to American English
-Translate Have you met Mrs Kalyani? to American English
-Translate Prof Joyner of King's College, London. to American English
-Translate Tea time is usually around 4 or 4.30. to American English
-Highlight translation in Mangoes are my favorite fruit.
-Highlight translation in I ate yogurt for breakfast.
-Highlight translation in We watched the footie match for a while.
-Highlight translation in Paracetamol takes up to an hour to work.
-*/
